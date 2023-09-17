bbfe24e59197a0b7258fcd86c6569da079dc949d
Alexander Kuznecov <alexander.kuznecov16@gmail.com>
2023-09-17 17:00:12 +0300

    куп

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-import 'expo-router/entry';
+import { registerRootComponent } from 'expo';
+import Layout from './app/_layout'; // Импортируйте компонент
+
+// Зарегистрируйте компонент как корневой
+registerRootComponent(Layout);
